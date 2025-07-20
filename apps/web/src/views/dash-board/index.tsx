0c188daad67b87932f1e32753e6a40c9ec2f1425
jialuma <jialuma@tencent.com>
2025-07-20 22:07:03 +0800

    feat(web): 引入vChart

--- apps/web/src/views/dash-board/index.tsx
+++ apps/web/src/views/dash-board/index.tsx
@@ -1,5 +1,28 @@
+import { VChart } from '@visactor/react-vchart';
 import type React from 'react';
+import { useRef, useState } from 'react';
 
+const getData = () =>
+  ['2:00', '4:00', '6:00', '8:00', '10:00', '12:00', '14:00', '16:00', '18:00'].map((time) => ({
+    time,
+    value: Math.random() * 10,
+  }));
+
+const getSpec = () => ({
+  type: 'line',
+  data: {
+    values: getData(),
+  },
+  xField: 'time',
+  yField: 'value',
+});
 export const DashBoard: React.FC = () => {
-  return <div>Dash Board</div>;
+  const [spec, setSpec] = useState(getSpec());
+  const chartRef = useRef(null);
+
+  return (
+    <div>
+      <VChart ref={chartRef} spec={spec} />
+    </div>
+  );
 };
